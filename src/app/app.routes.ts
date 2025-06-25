4064050cace6a08b1160bd08a9df9a99c652a567
Alejandro Feria Gonzalez <aferiag@unal.edu.co>
2025-06-24 21:31:58 -0500

    vista login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { Login } from './features/login/login';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
+  { path: 'login', component: Login},
   { path: '**', redirectTo: 'home' },
 ];
--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,11 +1,39 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { LucideAngularModule, Wind, ArrowLeft } from 'lucide-angular';
+import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [LucideAngularModule, RouterModule, ReactiveFormsModule],
   templateUrl: './login.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Login {
+export class Login implements OnInit {
+  readonly Wind = Wind;
+  readonly ArrowLeft = ArrowLeft;
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Form submitted', this.loginForm.value);
+      // Handle login logic here (e.g., API call)
+    } else {
+      // Mark all fields as touched to display errors
+      Object.values(this.loginForm.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
+  }
 }
