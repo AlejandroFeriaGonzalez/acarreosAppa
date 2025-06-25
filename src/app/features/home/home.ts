0531630f78a63c1ca524aa6fd95c93aebeb49005
Alejandro Feria Gonzalez <aferiag@unal.edu.co>
2025-06-24 20:37:12 -0500

    Hero secction y header

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Button } from "../../shared/components/button/button";
+import { RouterModule } from '@angular/router';
+import { LucideAngularModule, Wind } from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
-  imports: [Button],
+  imports: [Button, RouterModule, LucideAngularModule],
   templateUrl: './home.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Home {
-
+  readonly Wind = Wind;
 }
